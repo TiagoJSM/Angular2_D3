8359f602c7ed08b8eb493f95dd0f1a8add033a80
Tiago <tiago.martins180@gmail.com>
2016-08-04 16:46:21 +0100

    Changed css. Added axis to chart.

--- app/components/barChart/bar-chart.component.ts
+++ app/components/barChart/bar-chart.component.ts
@@ -19,43 +19,48 @@ export class BarChartComponent implements OnInit {
 			{name: "Fifth", value: 23}, 
 			{name: "Sixth", value: 42}];
 
-		var margin = {top: 20, right: 30, bottom: 30, left: 40},
+		const margin = {top: 20, right: 30, bottom: 30, left: 40},
 			width = 960 - margin.left - margin.right,
 			height = 500 - margin.top - margin.bottom;
 
 		const x = d3.scale.ordinal()
-					.domain(data.map(function(d) { return d.name; }))
-    				.rangeRoundBands([0, width], .1);
+			.domain(data.map(function(d) { return d.name; }))
+			.rangeRoundBands([0, width], .1);
 
 		const y = d3.scale.linear()
-					.domain([0, d3.max(data, function(d) { return d.value; })])
-    				.range([height, 0]);
+			.domain([0, d3.max(data, function(d) { return d.value; })])
+			.range([height, 0]);
 
 		const xAxis = d3.svg.axis()
-					.scale(x)
-					.orient("bottom");
+			.scale(x)
+			.orient("bottom");
+
+		const yAxis = d3.svg.axis()
+			.scale(y)
+			.orient("left");
 
 		const chart = d3.select(".chart")
-					.attr("width", width + margin.left + margin.right)
-    				.attr("height", height + margin.top + margin.bottom)
-				.append("g")
-    				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-		const bar = chart.selectAll("g")
-					.data(data)
-				.enter().append("g")
-					.attr("transform", function(d, i) { return "translate(" + x(d.name) + ",0)"; });
+		chart.append("g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(0," + height + ")")
+			.call(xAxis);
 
-		bar.append("rect")
-				.attr("y", function(d) { 
-					return y(d.value); })
-				.attr("height", function(d) { return height - y(d.value); })
-      			.attr("width", x.rangeBand());
+		chart.append("g")
+			.attr("class", "y axis")
+			.call(yAxis);
 
-		bar.append("text")
-				.attr("x", x.rangeBand() / 2)
-				.attr("y", function(d) { return y(d.value) + 3; })
-				.attr("dy", ".75em")
-				.text(function(d) { return d.value; });
+		chart.selectAll(".bar")
+			.data(data)
+			.enter().append("rect")
+			.attr("class", "bar")
+			.attr("x", function(d) { return x(d.name); })
+			.attr("y", function(d) { return y(d.value); })
+			.attr("height", function(d) { return height - y(d.value); })
+			.attr("width", x.rangeBand());
 	}
 }
